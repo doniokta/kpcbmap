5874f981844e24ff0fcd0f00ec4b3cb545755f27
Doni Oktavianus <56178638+doniokta@users.noreply.github.com>
2024-07-11 20:13:06 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,12 @@
-// Admin password
+// login.js
 const ADMIN_PASSWORD = "admin1234";
 
-// Handle login form submission
 document.getElementById('login-form').addEventListener('submit', function (event) {
   event.preventDefault();
   const passwordInput = document.getElementById('password').value;
 
   if (passwordInput === ADMIN_PASSWORD) {
-    document.getElementById('login-container').style.display = 'none';
-    document.getElementById('admin-content').style.display = 'flex';
-    
-    // Dynamically load admin.js
-    const script = document.createElement('script');
-    script.src = 'admin.js';
-    script.type = 'module';
-    document.body.appendChild(script);
+    window.location.href = "admin.html";
   } else {
     alert('Incorrect password. Please try again.');
   }
